6124edd4e2391ded8b3096d68430d2c6bd983a1b
m33ru <m33ru@pm.me>
2021-02-02 15:20:57 +0600

    failed

--- Challenge 1 - Basic UI Check/Basic UI Check.py
+++ Challenge 1 - Basic UI Check/Basic UI Check.py
@@ -1,14 +1,15 @@
+# importing webdriver
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
+# credentials
 username = "admin"
 password = "password"
 
+# chromedriver path
 driverLocation = "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2020.3.3\\lib\\chromedriver.exe"
 driver = webdriver.Chrome(driverLocation)
 driver.get("https://automationintesting.online/#/admin")
 driver.find_element_by_id("username").send_keys(username)
 driver.find_element_by_id("password").send_keys(password)
 driver.find_element_by_id("doLogin").click()
-
-element = driver.find_element_by_id("createRoom")
-print(element.is_enabled())
